21538a93a14337f98269af3747ef6da8f10d8f06
Nicholas Duncan <nickeduncan@gmail.com>
2017-07-19 19:20:58 -0400

    [7] study drills

--- ex7.py
+++ ex7.py
@@ -1,8 +1,13 @@
+# prints a string
 print("Mary had a little lamb.")
+# prints a formatted string
 print("Its fleece was white as {}.".format('snow'))
+# prints a string
 print("And everywhere that Mary went.")
+# prints "." 10 times
 print("." * 10) # what'd that do?
 
+# assigning characters to variables
 end1 = "C"
 end2 = "h"
 end3 = "e"
@@ -16,6 +21,7 @@ end10 = "g"
 end11 = "e"
 end12 = "r"
 
+# prints concatenated strings + a space
 # watch that comma at the end. try removing it to see what happens
 print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
 print(end7 + end8 + end9 + end10 + end11 + end12)

dadf7c030be05d5c0e3be0201b2940fe4c3ecac5
Nicholas Duncan <nickeduncan@gmail.com>
2017-07-20 07:38:35 -0400

    [9] study drills

--- ex9.py
+++ ex9.py
@@ -1,11 +1,16 @@
 # Here's some new strange stuff, remember to type it exactly.
 
+# sets variable to string of days
 days = "Mon Tue Wed Thu Fri Sat Sun"
+# sets variable to string of months, broken up with new lines"
 months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
 
+# prints the days variable
 print("Here are the days: ", days)
+# prints the months variable
 print("Here are the months: ", months)
 
+# prints a block quote
 print("""
 There's something goings on here.
 With the three double-quotes.

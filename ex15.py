ca60cbd76bb76fc0e39989cfd2b83ca316bd71db
Nicholas Duncan <nickeduncan@gmail.com>
2017-07-25 19:00:23 -0400

    [15] study drill comments

--- ex15.py
+++ ex15.py
@@ -1,15 +1,24 @@
+# includes the argv module
 from sys import argv
 
+# sets scripta and filename as variables from CLI
 script, filename = argv
 
+# opens the file and sets it to a var
 txt = open(filename)
 
+# prints text formatted with the filename
 print(f"Here's your file {filename}:")
+# prints the content of the file
 print(txt.read())
 
+# prompt
 print("Type the filename again:")
+# prompts for the filename
 file_again = input("> ")
 
+# opens the file inputted
 txt_again = open(file_again)
 
+# prints the content of the file
 print(txt_again.read())

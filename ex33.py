cefef0f35f3f63b6e0a274a316d7df59a2d61e3a
Nicholas Duncan <nickeduncan@gmail.com>
2017-08-04 21:27:41 -0400

    [33] study drills

--- ex33.py
+++ ex33.py
@@ -1,15 +1,18 @@
-i = 0
-numbers = []
+def numloop(max, add):
+    i = 0
+    numbers = []
 
-while i < 6:
-    print(f"At the top i is {i}")
-    numbers.append(i)
+    while i < max:
+        print(f"At the top i is {i}")
+        numbers.append(i)
 
-    i = i + 1
-    print("Numbers now: ", numbers)
-    print(f"At the bottom i is {i}")
+        i = i + add
+        print("Numbers now: ", numbers)
+        print(f"At the bottom i is {i}")
 
-print("The numbers: ")
+    print("The numbers: ")
 
-for num in numbers:
-    print(num)
+    for num in numbers:
+        print(num)
+
+numloop(8, 2)

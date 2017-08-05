6b1a229a9e279e85b167934c229f51e5250b29ba
Nicholas Duncan <nickeduncan@gmail.com>
2017-08-04 20:29:05 -0400

    [29] study drills

--- ex29.py
+++ ex29.py
@@ -14,13 +14,16 @@ if people < dogs:
 if people > dogs:
     print("The world is dry!")
 
-dogs += 5
-
 if people >= dogs:
     print("People are greater than or equal to dogs.")
 
 if people <= dogs:
     print("People are less than or equal to dogs.")
+
+if people != dogs:
+    print("People are not dogs.")
+
+dogs += 5
 
 if people == dogs:
     print("People are dogs.")

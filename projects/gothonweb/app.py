e2b3c312fd190acc6700f81a9035f8c417e4d282
Nicholas Duncan <nickeduncan@gmail.com>
2018-05-17 14:42:27 -0400

    [52] Created a simple web form

--- projects/gothonweb/app.py
+++ projects/gothonweb/app.py
@@ -1,12 +1,21 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/hello', methods=['POST', 'GET'])
 def index():
     greeting = "Hello World"
-    return render_template("index.html", greeting=greeting)
+
+    if request.method == "POST":
+        name = request.form['name']
+        greet = request.form['greet']
+        greeting = f"{greet}, {name}"
+        return render_template("index.html", greeting=greeting)
+    else:
+        return render_template("hello_form.html")
+
 
 @app.route('/foo')
 def foo():
